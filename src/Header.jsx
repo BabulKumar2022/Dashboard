dcf43f35fe0635856e033e9663e2274b16877d8f
unknown <bb3583400@gmail.com>
2023-09-16 00:04:48 +0600

    icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,20 @@
-
+import { BsFillBellFill, BsFillEnvelopeAtFill, BsJustify, BsPersonCircle, BsSearch} from 'react-icons/bs'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="header">
+        <div className="menu-icon">
+           <BsJustify className='icon'/>
+        </div>
+        <div className="header-left">
+            <BsSearch className='icon'/>
+        </div>
+        <div className="header-right">
+            <BsFillBellFill className='icon'/>
+            <BsFillEnvelopeAtFill className='icon'/>
+            <BsPersonCircle className='icon'/>
+        </div>
+    </header>
   )
 }
 
